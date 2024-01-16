e2457ce9e7ddaf4b869dfba5e42414278da41893
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-16 09:13:46 +0600

    Hero Section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <main className="home bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px]">
       <Navbar />
-      
+      <Hero />
     </main>
   );
 };
